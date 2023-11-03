89ed4db29188b9aca8da8dfdd6de4563735c4e1b
mateomckee <mateomckee64@gmail.com>
2023-11-02 19:32:44 -0500

    Created new pages and configured the routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { ResourcesComponent } from './resources/resources.component';
+import { CommunityComponent } from './community/community.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HomeComponent,
+    ResourcesComponent,
+    CommunityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about-us', component: AboutUsComponent},
+      { path: 'resources', component: ResourcesComponent},
+      { path: 'community', component: CommunityComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
